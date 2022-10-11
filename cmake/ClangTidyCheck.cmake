4cf48885907c373d38498d48a058303f2c937a05
Dmitriy Baranov <amareelez@gmail.com>
2022-10-12 01:56:58 +0400

    Search for clang-tidy binary only if needed

--- cmake/ClangTidyCheck.cmake
+++ cmake/ClangTidyCheck.cmake
@@ -1,13 +1,13 @@
 option(CLANG_TIDY_CHECK "Run clang-tidy before the build")
 
-find_program(CLANG_TIDY_EXE
+if (CLANG_TIDY_CHECK)
+    find_program(CLANG_TIDY_EXE
     NAMES
         "clang-tidy-12"
         "clang-tidy-6.0"
         "clang-tidy"
     REQUIRED
-)
+    )
 
-if (CLANG_TIDY_CHECK)
     set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY_EXE}")
 endif()
