308983eb28d045d64eed2a7216dff339622cc114
Kalabuk Dzmitry <kalabukdima@gmail.com>
2022-11-24 10:56:52 +0300

    Add declarations to 08_population template

--- 08_population/Program.h
+++ 08_population/Program.h
@@ -5,4 +5,8 @@ public:
     void readWorldCountriesFromFile(
         const std::string& fileCountries, const std::string& filePopulation, char delimiter
     );
+
+    void writeWorldPopulationToFile(const std::string& file, char delimiter);
+
+    void writeWorldRegionPopulationToFile(const std::string& file, char delimiter);
 };
